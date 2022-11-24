af0afe39f33f37b0bd1dc8a3ea3f745f476e9a23
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-11-24 12:13:41 +0600

    daisy ui added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-blue-500 text-4xl font-semibold">Resellx</h1>
-      <p>buy and sell laptop easily</p>
+      <p className="text-red-500 text-xl">buy and sell laptop easily</p>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,30 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#e11d48",
+
+          secondary: "#bbf7d0",
+
+          accent: "#fecdd3",
+
+          neutral: "#282D33",
+
+          "base-100": "#FAF8FF",
+
+          info: "#86BFE4",
+
+          success: "#74ECB4",
+
+          warning: "#F7B355",
+
+          error: "#E03E71",
+        },
+      },
+    ],
+  },
 };
