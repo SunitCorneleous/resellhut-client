513e087f80d8ac7ee67ccfffd75197f835d94057
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-11-24 19:00:04 +0600

    categories added with axios api call

--- src/Pages/Home/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories.js
@@ -1,23 +1,30 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/categories")
+      .then(res => setCategories(res.data));
+  }, []);
+
   return (
     <div className="p-10 md:my-32 my-16">
       <h1 className="text-center text-primary text-2xl md:text-4xl font-bold">
         Browse Laptops by Categories
       </h1>
       <div className="w-10/12 mx-auto my-4 flex justify-around mt-12 flex-col md:flex-row">
-        <Link className="w-52 border-4 text-2xl font-medium mb-3 md:mb-0 border-primary mx-auto py-3 px-1 rounded-md text-center hover:bg-primary text-primary  hover:text-warning cursor-pointer">
-          Budget
-        </Link>
-
-        <Link className="w-52 border-4 mb-3 md:mb-0 border-primary mx-auto py-3 px-1 rounded-md text-center hover:bg-primary text-primary  hover:text-warning cursor-pointer">
-          <p className="text-2xl font-medium">Budget</p>
-        </Link>
-        <Link className="w-52 border-4 border-primary mx-auto py-3 px-1 rounded-md text-center hover:bg-primary text-primary  hover:text-warning cursor-pointer">
-          <p className="text-2xl font-medium">Budget</p>
-        </Link>
+        {categories.map(category => (
+          <Link
+            key={category._id}
+            className="w-52 border-4 text-2xl font-medium mb-3 md:mb-0 border-primary mx-auto py-3 px-1 rounded-md text-center hover:bg-primary text-primary  hover:text-warning cursor-pointer"
+          >
+            {category.name}
+          </Link>
+        ))}
       </div>
     </div>
   );
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
+import Category from "../../Pages/Category/Category/Category";
 import Home from "../../Pages/Home/Home/Home";
 
 export const router = createBrowserRouter([
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "/category/:id",
+        element: <Category></Category>,
       },
     ],
   },
